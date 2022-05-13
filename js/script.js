b133a9fa329cbd34692cd92f8a7e7e2202dbf282
alexcapoluongo <alexcapoluongo@icloud.com>
2022-05-13 14:22:19 +0200

    first trial

--- js/script.js
+++ js/script.js
@@ -1,9 +1,31 @@
+// Attraverso l'apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
 const app = new Vue (
     {
         el: "#root",
 
         data:{
+            email: "",
+            emailsArray: [],
+            min: 0,
+            max: 10
+        },
+
+        methods: {
+            generateEmail() {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) => {            
+                    this.email = response.data.response        
+                })
+            },
+
             
+            generateArray() {
+            }
+
         }
+
     }
 )
