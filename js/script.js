8860fc57630e0760775fc156352590f2f400c5d4
alexcapoluongo <alexcapoluongo@icloud.com>
2022-05-16 09:51:34 +0200

    improved and shortened  code

--- js/script.js
+++ js/script.js
@@ -8,27 +8,37 @@ const app = new Vue (
 
         data:{
             emailsArray: [],
-            isVisible: false
+        },
+
+        //prova con il created e non il method
+        created() {
+            for(let i= 0; i < 10; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) => {
+                    const newEmail = response.data.response
+                    this.emailsArray.push(newEmail);
+                })
+            }
         },
 
         methods: {
-            generateEmail() {
-                for(let i= 0; i < 10; i++) {
-                    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-                    .then((response) => {
-                        this.emailsArray.push(response.data.response)
-                        this.showList()  
-                    })
-                } 
-                this.emailsArray = []
-            },
+            // generateEmail() {
+            //     for(let i= 0; i < 10; i++) {
+            //         axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            //         .then((response) => {
+            //             this.emailsArray.push(response.data.response)
+            //             this.showList()  
+            //         })
+            //     } 
+            //     this.emailsArray = []
+            // },
 
             
-            showList() {
-                if(this.emailsArray.length > 9) {
-                    this.isVisible = true;
-                }
-            }
+            // showList() {
+            //     if(this.emailsArray.length > 9) {
+            //         this.isVisible = true;
+            //     }
+            // }
 
         }
 
